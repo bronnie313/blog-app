8eb881107f42d0dc62c62dfb4ce5d3f1bee73016
Bisaso Ronald <bronnie313@gmail.com>
2023-08-16 20:24:24 +0300

    fix errors in the shema

--- db/migrate/20230816161917_create_like.rb
+++ db/migrate/20230816161917_create_like.rb
@@ -8,6 +8,6 @@ class CreateLike < ActiveRecord::Migration[7.0]
     end
 
     add_foreign_key :likes, :users, column: :user_id
-    add_foreign_key :likes, :posts, column: :post_id 
+    add_foreign_key :likes, :posts, column: :post_id
   end
 end

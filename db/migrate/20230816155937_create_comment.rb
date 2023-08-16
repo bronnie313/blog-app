831e81068cea1f91be846e10a4ccf5646d53fc22
Bisaso Ronald <bronnie313@gmail.com>
2023-08-16 19:33:18 +0300

    add migrations to create tables

---
+++ db/migrate/20230816155937_create_comment.rb
@@ -0,0 +1,14 @@
+class CreateComment < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :post_id
+      t.text :text
+
+      t.timestamps
+    end
+
+    add_foreign_key :comments, :users, columm: :user_id
+    add_foreign_key :comments, :posts, columm: :post_id
+  end
+end

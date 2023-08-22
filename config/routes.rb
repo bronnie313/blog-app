e6bcfd5b140447a7a45c3c86e8a683ff3e93266a
Bisaso Ronald <bronnie313@gmail.com>
2023-08-22 12:02:23 +0300

    add routes for the required views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root 'pages#hello'
+  # root 'pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/posts/:id', to: 'posts#show', as: 'user_post'
 end

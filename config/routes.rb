96eb96abb0ecb445ab813b879aa904f70078a619
Bisaso Ronald <bronnie313@gmail.com>
2023-08-23 12:50:24 +0300

    add test to the user_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # root 'pages#hello'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   root 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe UsersController, type: :controller do
       get :index
       expect(response).to be_successful
     end
+
+    it 'renders a correct template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns the correct placeholder text in the response body' do
+      get :index
+      expect(response.body).to include("List of Users")
+    end
   end
 end

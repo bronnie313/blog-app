f21cfdcc724d555642bdc7cf625f2eda1ffbd855
Bisaso Ronald <bronnie313@gmail.com>
2023-08-24 13:05:00 +0300

    add modification to the tests

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -1,37 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe PostsController, type: :request do
   describe 'GET #index' do
-    it 'returns a successive response' do
-      get :index
+    it 'returns a success response' do
+      user = User.create(id: 1)
+      get user_posts_path(user_id: user.id)
       expect(response).to be_successful
     end
 
     it 'renders a correct template' do
-      get :index
+      user = User.create(id: 1)
+      get user_posts_path(user_id: user.id)
       expect(response).to render_template(:index)
     end
 
     it 'returns the correct placeholder text in the response body' do
-      get :index
-      expect(response.body).to include('')
+      user = User.create(id: 1)
+      get user_posts_path(user_id: user.id)
+      expect(response.body).to include('Posts by User')
     end
   end
 
   describe 'GET #show' do
     it 'returns a successful response' do
-      get :show, params: { id: 1 }
+      get user_post_path(user_id: 1, id: 1)
       expect(response).to be_successful
     end
   end
 
   it 'renders the show template' do
-    get :show, params: { id: 1 }
+    get user_post_path(user_id: 1, id: 1)
     expect(response).to render_template(:show)
   end
 
   it 'includes correct placeholder text in the response body' do
-    get :show, params: { id: 1 }
-    expect(response.body).to include('')
+    get user_post_path(user_id: 1, id: 1)
+    expect(response.body).to include('Single Post')
   end
 end
--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
+RSpec.describe UsersController, type: :request do
   describe 'GET #index' do
-    it 'returns a success response' do
-      get :index, params: { user_id: 1 }
+    it 'returns a successive response' do
+      get '/users'
       expect(response).to be_successful
     end
 
     it 'renders a correct template' do
-      get :index, params: { user_id: 1 }
+      get '/users'
       expect(response).to render_template(:index)
     end
 
     it 'returns the correct placeholder text in the response body' do
-      get :index, params: { user_id: 1 }
-      expect(response.body).to include('')
+      get '/users'
+      expect(response.body).to include('List of Users')
     end
   end
 
   describe 'GET #show' do
     it 'returns a successful response' do
-      get :show, params: { user_id: 1, id: 1 }
+      get user_path(1)
       expect(response).to be_successful
     end
   end
 
   it 'renders the show template' do
-    get :show, params: { user_id: 1, id: 1 }
+    get user_path(1)
     expect(response).to render_template(:show)
   end
 
   it 'includes correct placeholder text in the response body' do
-    get :show, params: { user_id: 1, id: 1 }
-    expect(response.body).to include('')
+    get user_path(1)
+    expect(response.body).to include('User Profile')
   end
 end

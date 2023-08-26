6337fd5c17eecc72112c955c0803b2775cd14a90
Bisaso Ronald <bronnie313@gmail.com>
2023-08-26 11:31:25 +0300

    add new and create methods to the posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.user = current_user
+    
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 end

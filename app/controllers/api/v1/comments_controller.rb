33ddb826c6d5116c56fae255ec42bd15b2a1056b
AleWaweru <ngashalex8@gmail.com>
2023-09-09 21:35:00 +0300

    Added index method for the api endpoint

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+        user = User.find(params[:user_id])
+        post = user.posts.find(params[:post_id])
+        comments = post.comments
+        if comments.present?
+          render json: { success: true, message: 'Comments retrieved successfully', posts: comments }
+        else
+          render json: { success: false, message: 'No comments found for the user', status: :not_found }
+        end
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'User, post, or comments not found' }, status: :not_found
+      end
 end
